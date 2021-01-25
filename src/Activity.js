f22e43614d6da8da5341cbc366ff4b85c9eb01d2
hamideshoun <hamid.naraghi@gmail.com>
2021-01-25 13:29:00 +0330

    Delete src directory

--- src/Activity.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function Activity() {
-  return (
-    <h1>Activity</h1>
-  );
-}
-
-export default Activity;
--- src/Settings.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function Settings() {
-  return (
-    <h1>Settings</h1>
-  );
-}
-
-export default Settings;
