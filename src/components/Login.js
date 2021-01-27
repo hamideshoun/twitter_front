dae86b4b3b32daf75e832141526dd2417d3f464a
HAMID <hamid.naraghi@gmail.com>
2021-01-27 13:25:50 +0330

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,11 +10,34 @@ const inputStyle = {
 };
 
 class Login extends Component{
-    submit = (username, password) => {
-        if (username.length() < 4){
-            
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         password: "",
+    //         username: "",
+    //     }
+    // }
+
+    submit = (Username, Password) => {
+        if (Username.length < 4){
+            alert("usermame must be more than 4 characters");
+        }
+        else {
+            this.fetchItems(Username, Password);
         }
     }
+
+    // componentDidMount(){
+    //     this.fetchItems();
+    // };
+
+    // function to send request and set state = response
+    fetchItems = async () => {
+        const data = await fetch(
+            {}
+        );
+        const items = await data.json();
+    };
 
     render(){
         return(
@@ -23,9 +46,9 @@ class Login extends Component{
                 <form style={{display: 'grid', justifyItems: 'center', alignItems: 'center', alignSelf: 'flex-start'}} id="login-form">
                     <input style={inputStyle} type="text" name="username" id="username-field" className="login-form-field" placeholder="Username" id="username" />
                     <input style={inputStyle} type="password" name="password" id="password-field" className="login-form-field" placeholder="Password" id="password" />
-                    <input style={inputStyle} type="submit" value="Login" id="login-form-submit"  
+                    <input style={inputStyle} type="button" value="Login" id="login-form-submit"
                         onClick={() => this.submit(document.getElementById("username").value, 
-                                                    document.getElementById("password").value)} 
+                                    document.getElementById("password").value)} 
                     />
                 </form>
             </div>
